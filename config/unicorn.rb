ac828c94cc0c2a69dd36cbc00825576658ed0cc1
David Ladowitz <david@ladowitz.com>
2014-08-01 20:51:01 -0700

    testing slug size

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,6 +10,7 @@ before_fork do |server, worker|
 
   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
 
+  
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
 end
